a37e1d4e2bf89c3895a6a768974318dde3a5462b
KaewSathita <kaew.gothai@gmail.com>
2022-09-20 15:16:24 +0700

    done login

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,10 +1,12 @@
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { Op } = require('Sequelize');
 
 
 const AppError = require("../utils/appError");
 const { User } = require('../models');
+
 
 const genToken = payload => 
   jwt.sign(payload, process.env.JWT_SECRET_KEY|| 'private_key', { 
@@ -50,4 +52,34 @@ exports.register = async ( req, res, next ) => {
   }   catch (err) {
       next (err);
   }
+};
+
+exports.login = async ( req, res, next ) => {
+  try {
+    const { emailOrMobile, password } = req.body;
+
+    if (typeof emailOrMobile !== 'string' || typeof password !== 'string') {
+      throw new AppError('email address or mobile or password is invalid', 400);
+    }
+    const user = await User.findOne({ 
+      where: {
+        [Op.or]: [{ email: emailOrMobile }, { mobile: emailOrMobile }]
+      }
+    });
+
+    if (!user) {
+      throw new AppError('email address or mobile or password is invalid', 400);
+    }
+
+    const isCorrect = await bcrypt.compare(password, user.password)
+    if(!isCorrect) {
+      throw new AppError('email address or mobile or password is invalid', 400);
+    }
+
+    const token = genToken({ id: user.id });
+      res.status(200).json({token});
+
+    } catch (err) {
+      next(err)    
+    }
 };
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/authControllers')
 const router = express.Router()
 
 router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 module.exports = router;
