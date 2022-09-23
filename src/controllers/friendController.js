eecbf159eeb39b4400184036a098cb384aa9cc1f
KaewSathita <kaew.gothai@gmail.com>
2022-09-23 15:05:05 +0700

    add feature request and accept friend

--- src/controllers/friendController.js
+++ src/controllers/friendController.js
@@ -1,7 +1,7 @@
 const { Op } = require('sequelize');
 const { Friend } = require('../models');
 const AppError = require('../utils/appError')
-const {FRIEND_PENDING} = require('../config/constants')
+const {FRIEND_PENDING, FRIEND_ACCEPTED} = require('../config/constants')
 
 exports.deleteFriend = async ( req, res, next ) => {
   try {
@@ -51,8 +51,20 @@ exports.createFriend = async (req, res, next) => {
 }
 exports.updateFriend = async (req, res, next) => {
   try {
-
+    const { friendId } = req.params;
+    // SELECT * FROM friends WHERE status = 'PENDING'
+    // AND accepter_id = req.user.id AND requester_id = friendID 
+    await Friend.update({ status: FRIEND_ACCEPTED}, 
+      {
+        where: { 
+          status: FRIEND_PENDING, 
+          requesterId: friendId, 
+          accepterId: req.user.id
+        }
+      }
+    );
+    res.status(200).json({ message: 'success add friend'});
   } catch (err){
-    next(err)
+    next(err);
   }
-}
+};
